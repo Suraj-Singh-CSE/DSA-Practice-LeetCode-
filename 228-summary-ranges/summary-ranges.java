0d727064135161ad592260f86b985ce985590f9a
Suraj Singh <117113315+Suraj-Singh-CSE@users.noreply.github.com>
2025-01-25 23:45:53 +0530

    Time: 5 ms (81.06%) | Memory: 41.9 MB (34.54%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -3,23 +3,19 @@ class Solution {
         List<String> ranges = new ArrayList<>();
 
         if (nums.length == 0) {
-            return ranges;  // Return an empty list if the array is empty
+            return ranges;
         }
 
-        int start = nums[0];  // Initialize the start of the current range
+        int start = nums[0];
 
         for (int i = 1; i <= nums.length; i++) {
-            // If the current number is not consecutive or we've reached the end of the array
             if (i == nums.length || nums[i] != nums[i - 1] + 1) {
                 if (start == nums[i - 1]) {
-                    // If start and end are the same, it's a single number
                     ranges.add(String.valueOf(start));
                 } else {
-                    // Otherwise, it's a range from start to the previous number
                     ranges.add(start + "->" + nums[i - 1]);
                 }
 
-                // Start a new range if there are more numbers
                 if (i < nums.length) {
                     start = nums[i];
                 }
